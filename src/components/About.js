ac39fe809b4aa35c69269b5027180410bea930d4
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-12 22:55:48 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { image } from "../data/user";
 function About() {
   return (
     <div id="about">
@@ -9,3 +10,4 @@ function About() {
     </div>
   );
 }
+export default About; 
